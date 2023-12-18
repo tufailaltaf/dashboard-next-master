5dd326af9aa48fff89cac62f1ed3e7cd9d11c6d8
tufailaltaf <tufailaltaf11@gmail.com>
2023-12-19 00:36:51 +0500

    edit

--- app/api/create_customer/route.ts
+++ app/api/create_customer/route.ts
@@ -1,16 +1,50 @@
+// import prismadb from "@/app/lib/prismadb";
+// import { customers } from "@/app/lib/tables";
+
+// import { NextRequest, NextResponse } from 'next/server';
+
+// export async function GET(request: NextRequest) {
+  
+
+//   const customerdata = await prismadb.customers.createMany({
+//     data: customers,
+//   });
+
+//   const responseBody = JSON.stringify(customerdata);
+//   return NextResponse.json({
+//     message: responseBody,
+//     status: 200,
+//     statusText: 'OK',
+//     headers: {
+//       'Access-Control-Allow-Origin': '*',
+//       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+//     },
+//     body: request,
+//   });
+// }
 import prismadb from "@/app/lib/prismadb";
 import { customers } from "@/app/lib/tables";
 
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  
+  // Validate uniqueness of email addresses
+  const uniqueEmails = new Set(customers.map(customer => customer.email));
+  if (uniqueEmails.size !== customers.length) {
+    // Handle duplicate email addresses
+    return NextResponse.json({
+      message: 'Duplicate email addresses found',
+      status: 400,
+      statusText: 'Bad Request',
+    });
+  }
 
-  const customerdata = await prismadb.customers.createMany({
+  const customerData = await prismadb.customers.createMany({
     data: customers,
   });
 
-  const responseBody = JSON.stringify(customerdata);
+  const responseBody = JSON.stringify(customerData);
   return NextResponse.json({
     message: responseBody,
     status: 200,
