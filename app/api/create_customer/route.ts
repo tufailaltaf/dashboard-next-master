d8583b77b3225be1422e47357e7276a5199d3061
tufailaltaf <tufailaltaf11@gmail.com>
2023-12-19 02:12:37 +0500

    update

--- app/api/create_customer/route.ts
+++ app/api/create_customer/route.ts
@@ -1,26 +1,26 @@
-'use server'
-import prismadb from "@/app/lib/prismadb";
-import { customers } from "@/app/lib/tables";
+// 'use server'
+// import prismadb from "@/app/lib/prismadb";
+// import { customers } from "@/app/lib/tables";
 
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
+// export async function GET(request: NextRequest) {
 
 
-  const customerdata = await prismadb.customers.createMany({
-    data: customers,
-  });
+//   const customerdata = await prismadb.customers.createMany({
+//     data: customers,
+//   });
 
-  const responseBody = JSON.stringify(customerdata);
-  return NextResponse.json({
-    message: responseBody,
-    status: 200,
-    statusText: 'OK',
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    },
-    body: request,
-  });
-}
+//   const responseBody = JSON.stringify(customerdata);
+//   return NextResponse.json({
+//     message: responseBody,
+//     status: 200,
+//     statusText: 'OK',
+//     headers: {
+//       'Access-Control-Allow-Origin': '*',
+//       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+//     },
+//     body: request,
+//   });
+// }
--- app/dashboard/customers/createcustomer/page.tsx
+++ app/dashboard/customers/createcustomer/page.tsx
@@ -1,22 +1,22 @@
-'use client'
-import { get_response } from "@/actions/response"
-import { useEffect, useState } from "react"
-export default async function Invoices() {
+// 'use client'
+// import { get_response } from "@/actions/response"
+// import { useEffect, useState } from "react"
+// export default async function Invoices() {
   
-// const [Invoice, setInvoice] = useState([])
-//   useEffect(() => {
-//     async function getData() {
-//       const res = await fetch(`../../api/create_customer`)
-//       const data = await res.json()
-//       //return data
-//       setInvoice(data)
-//     }
-//     getData()
-//   },[])
-const {data} = get_response(`/api/create_customer`)
-  //const data = await getData()
-  //console.log(Invoice)
-  return <main>
-    <h1>Create Invoice</h1>
-  </main>
-}
+// // const [Invoice, setInvoice] = useState([])
+// //   useEffect(() => {
+// //     async function getData() {
+// //       const res = await fetch(`../../api/create_customer`)
+// //       const data = await res.json()
+// //       //return data
+// //       setInvoice(data)
+// //     }
+// //     getData()
+// //   },[])
+// const {data} = get_response(`/api/create_customer`)
+//   //const data = await getData()
+//   //console.log(Invoice)
+//   return <main>
+//     <h1>Create Invoice</h1>
+//   </main>
+// }
